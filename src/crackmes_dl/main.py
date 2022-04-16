e132eb9da73a008c1188e5db5316f84a44092144
Kristian Nymann Jakobsen <kristian@nymann.dev>
2022-04-16 08:47:19 +0200

    :construction: Throwing stuff at the wall

--- src/crackmes_dl/main.py
+++ src/crackmes_dl/main.py
@@ -1,11 +1,20 @@
 import typer
 
+from crackmes_dl.api import AuthPayload
+from crackmes_dl.api import CrackmesApi
+from crackmes_dl.api import SearchPayload
+
 app = typer.Typer()
 
 
 @app.command()
-def welcome(name: str) -> None:
-    typer.echo(f"Welcome {name}!")
+def download(username: str, password: str, domain: str = "https://crackmes.one") -> None:
+    api = CrackmesApi(domain=domain)
+    creds = AuthPayload(name=username, password=password)
+    if not api.login(payload=creds):
+        raise Exception("Login failed")
+    search_terms = SearchPayload(name="ransom")
+    api.search(payload=search_terms)
 
 
 if __name__ == "__main__":
