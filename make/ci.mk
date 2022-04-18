624a2a53df557f19a206f12ecf6e28cb01a25952
Kristian Nymann Jakobsen <kristian@nymann.dev>
2022-04-18 03:15:38 +0200

    :construction_worker: Add docker push ci

--- make/ci.mk
+++ make/ci.mk
@@ -1,20 +1,22 @@
-ONBUILD?=${CI_REGISTRY_IMAGE}:onbuild
+DOCKER_REPO?=nymann/crackmes-dl
+DOCKER_TAG?=${DOCKER_REPO}:${VERSION}
 
 package: ${VERSION} setup.py
 	@python setup.py sdist bdist_wheel
 
-docker-build-onbuild-ci: ${VERSION}
+docker-build: ${VERSION}
 	@docker build -f docker/Dockerfile .
 
-docker-push-onbuild-ci: ${VERSION}
+docker-push: ${VERSION}
 	@docker build \
 		--cache-from ${ONBUILD} \
-		-t ${ONBUILD} \
+        -t ${DOCKER_REPO}:latest \
+        -t ${DOCKER_REPO}:${VERSION} \
 		-f docker/Dockerfile .
-	@docker push ${CI_REGISTRY_IMAGE}
+	@docker push ${DOCKER_TAG}
 
 version-requirements: ${VERSION}
 	# This is used as a precursor to building images via the pipeline
 	@echo "${VERSION}"
 
-.PHONY:docker-build-onbuild-ci docker-push-onbuild-ci version-requirements package
+.PHONY:docker-build docker-push version-requirements package
